233a4864a94ad902f431b2e45b1987aedd3dcecb
Jason Zhou <24jasonz@students.tas.tw>
2024-01-17 18:47:52 +0800

    add pathplanner event marker support

--- src/main/java/raidzero/robot/auto/actions/WaitForEventMarkerAction.java
+++ src/main/java/raidzero/robot/auto/actions/WaitForEventMarkerAction.java
@@ -1,16 +1,13 @@
 package raidzero.robot.auto.actions;
 
-import java.util.List;
+import com.pathplanner.lib.path.EventMarker;
+import com.pathplanner.lib.path.PathPlannerPath;
 
-// import com.pathplanner.lib.PathPlannerTrajectory;
-// import com.pathplanner.lib.PathPlannerTrajectory.EventMarker;
-
-import edu.wpi.first.wpilibj.Timer;
+import raidzero.robot.submodules.Swerve;
 
 public class WaitForEventMarkerAction implements Action {
-    private Timer mTimer = new Timer();
-    // private List<EventMarker> mMarkers;
-    private double mWaitTime;
+    private EventMarker mMarker;
+    private static final Swerve mSwerve = Swerve.getInstance();
 
     /**
      * Action that blocks until event marker has been reached
@@ -19,22 +16,13 @@ public class WaitForEventMarkerAction implements Action {
      * @param name        marker name
      * @param currentTime current time in the trajectory
      */
-    // public WaitForEventMarkerAction(PathPlannerTrajectory trajectory, String name, double currentTime) {
-    //     mMarkers = trajectory.getMarkers();
-    //     double wait = 0.0;
-    //     for (EventMarker marker : mMarkers) {
-    //         if (marker.names.get(0).equals(name)) {
-    //             wait = marker.timeSeconds - currentTime;
-    //         }
-    //     }
-    //     mWaitTime = wait > 0 ? wait : 0;
-    // }
+    public WaitForEventMarkerAction(PathPlannerPath trajectory, int index) {
+        mMarker = trajectory.getEventMarkers().get(index);
+    }
 
     @Override
     public void start() {
         System.out.println("[Auto] Action '" + getClass().getSimpleName() + "' started!");
-        mTimer.reset();
-        mTimer.start();
     }
 
     @Override
@@ -44,11 +32,10 @@ public class WaitForEventMarkerAction implements Action {
     @Override
     public void done() {
         System.out.println("[Auto] Action '" + getClass().getSimpleName() + "' finished!");
-        mTimer.stop();
     }
 
     @Override
     public boolean isFinished() {
-        return mTimer.hasElapsed(mWaitTime);
+        return mMarker.shouldTrigger(mSwerve.getPose());
     }
 }
