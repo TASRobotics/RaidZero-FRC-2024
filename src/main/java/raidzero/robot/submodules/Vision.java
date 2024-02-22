bb799229442a7f2600ae960da507955c3582c05f
24thomast <24thomast@students.tas.tw>
2024-02-22 18:20:16 +0800

    February 22nd: Autoaim Vision

--- src/main/java/raidzero/robot/submodules/Vision.java
+++ src/main/java/raidzero/robot/submodules/Vision.java
@@ -2,18 +2,21 @@ package raidzero.robot.submodules;
 
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Timer;
 
 import raidzero.robot.Constants.VisionConstants;
 import raidzero.robot.wrappers.LimelightHelpers;
-import raidzero.robot.wrappers.LimelightHelpers.LimelightResults;
 import raidzero.robot.wrappers.LimelightHelpers.Results;
 
 public class Vision extends Submodule {
     private static Vision instance;
     private static final Swerve drive = Swerve.getInstance();
+
+    private Pose2d visionPose = new Pose2d();
 
     public static Vision getInstance() {
         if (instance == null) {
@@ -35,17 +38,34 @@ public class Vision extends Submodule {
 
     @Override
     public void update(double timestamp) {
-        updateVisionMeasurement();
+        SmartDashboard.putNumber("Vision X", visionPose.getX());
+        SmartDashboard.putNumber("Vision Y", visionPose.getY());
+        SmartDashboard.putBoolean("Vision Target", hasTarget());
+        SmartDashboard.putNumber("Speaker Distance", getSpeakerDistance(Alliance.Blue));
+        try {
+            SmartDashboard.putNumber("Speaker Angle", getSpeakerAngle(Alliance.Blue).getDegrees());
+        } catch (Exception e) {  
+        } 
+        updatePose();
     }
 
     @Override
     public void stop() {
     }
 
+    public void updatePose() {
+        Results results = LimelightHelpers.getLatestResults(VisionConstants.NAME).targetingResults;
+
+        Pose2d robotPose = results.getBotPose2d_wpiBlue();
+        if (hasTarget()) {
+            visionPose = robotPose;
+        }
+    }
+
     public void updateVisionMeasurement(){
         Results results = LimelightHelpers.getLatestResults(VisionConstants.NAME).targetingResults;
         
-        Pose2d robotPose = results.getBotPose2d();
+        Pose2d robotPose = results.getBotPose2d_wpiBlue();
         double tl = results.latency_pipeline;
         double cl = results.latency_capture;
         
@@ -59,11 +79,23 @@ public class Vision extends Submodule {
         return LimelightHelpers.getTV(VisionConstants.NAME);
     }
 
+    public Pose2d getVisionPose(){
+        return visionPose;
+    }
+
     public double getSpeakerDistance(Alliance alliance) {
         Pose2d speakerPose = alliance == Alliance.Blue ? VisionConstants.BLUE_SPEAKER : VisionConstants.RED_SPEAKER;
         if (!hasTarget()){
             return 0;
         }
-        return drive.getPose().getTranslation().getDistance(speakerPose.getTranslation());
+        return visionPose.getTranslation().getDistance(speakerPose.getTranslation());
+    }
+
+    public Rotation2d getSpeakerAngle(Alliance alliance) {
+        Pose2d speakerPose = alliance == Alliance.Blue ? VisionConstants.BLUE_SPEAKER : VisionConstants.RED_SPEAKER;
+        if (!hasTarget()){
+            return null;
+        }
+        return Rotation2d.fromRadians(Math.atan2(visionPose.getY() - speakerPose.getY(), visionPose.getX() - speakerPose.getX()));
     }
 }
